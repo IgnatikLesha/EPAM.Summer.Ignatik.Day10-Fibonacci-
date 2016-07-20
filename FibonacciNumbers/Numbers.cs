80daf4712a73eef0e46ff39a894873c841af1462
Alexey Ignatik <ignatiklesha@gmail.com>
2016-07-20 17:30:52 +0300

    Add project files.

---
+++ FibonacciNumbers/Numbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciNumbers
+{
+    public static class Numbers
+    {
+        public static int Fibonacci(int n)
+        {
+            return n > 1 ? Fibonacci(n - 1) + Fibonacci(n - 2) : n;
+        }
+
+        public static IEnumerator GetNumbers(int n)
+        {
+            if(n <=0 )
+                throw new ArgumentException();
+
+            for (int i = 0; i < n; i++)
+            {
+                yield return Fibonacci(i);
+            }
+        }
+
+    }
+}
